ac0d3a9c581d50ada3ac02b7147343a0a7983ba9
almcr <alm.dev2@gmail.com>
2022-07-20 10:13:25 +0200

    feat[day1]: completed day1

---
+++ src/bin/day1/main.rs
@@ -0,0 +1,21 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use itertools::Itertools;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+  // Read the input file into buffer
+  let file = File::open("src/bin/day1/input")?;
+  let reader = BufReader::new(file);
+
+  // Iterate over the buffer and sum the numbers larger than previous ones
+  let lines = reader.lines();
+
+  let res = lines.map(|l| l.unwrap().parse::<u16>().unwrap())
+  .tuple_windows()
+  .filter(|(a, b)| a < b)
+  .count();
+
+  println!("{}", res);
+
+  Ok(())
+}
